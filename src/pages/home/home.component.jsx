7603fd71f7c6c94f1ac6597597e1d3574626dfbc
Orlando Arnosa <oarnosa@gmail.com>
2021-03-29 11:36:46 -0400

    Update pricing

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -127,7 +127,7 @@ const Home = () => {
             <Plan
               icon="https://svgur.com/i/UkH.svg"
               name="STEEL"
-              price="$50"
+              price="$80"
               time="60"
             >
               <li>Manual treatment with focus on 2 problem areas</li>
@@ -140,7 +140,7 @@ const Home = () => {
               special
               icon="https://svgur.com/i/V1U.svg"
               name="GOLD"
-              price="$75"
+              price="$100"
               time="90"
             >
               <li>Manual treatment with focus on 3 problem areas</li>
@@ -154,7 +154,7 @@ const Home = () => {
             <Plan
               icon="https://svgur.com/i/Ujt.svg"
               name="OBSIDIAN"
-              price="$100"
+              price="$125"
               time="120"
             >
               <li>Manual treatment with focus on 4 problem areas</li>
