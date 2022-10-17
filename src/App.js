c7a7c1f37f2cc946bff8142b9e0e79534d27a024
Orlando Arnosa <oarnosa@gmail.com>
2022-10-17 12:41:12 -0400

    Changes to pricing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Navigate } from 'react-router-dom';
 
-import Home from "./pages/home/home.component.jsx";
+import Home from './pages/home/home.component.jsx';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faFacebook,
   faInstagram,
-  faLinkedinIn
-} from "@fortawesome/free-brands-svg-icons";
-import {
-  faStar
-} from "@fortawesome/free-solid-svg-icons";
-
-
-library.add(
-  faFacebook,
   faLinkedinIn,
-  faInstagram,
-  faStar,
-);
+} from '@fortawesome/free-brands-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faFacebook, faLinkedinIn, faInstagram, faStar);
 
 function App() {
   return (
     <Router>
       <Route path="/">
-        <Home/>
+        <Home />
       </Route>
-      <Redirect to='/'/>
+      <Navigate to="/" />
     </Router>
   );
 }
--- src/components/plan/plan.component.jsx
+++ src/components/plan/plan.component.jsx
@@ -8,10 +8,6 @@ const Plan = ({ special, icon, name, price, priceMonthly, time, children }) => (
     <div className="plan__content">
       <img className="plan__icon" src={icon} alt={name} />
       <h3 className="plan__title">{name}</h3>
-      <h1 className="plan__price">
-        {priceMonthly}
-        <span className="plan__time"> / month</span>
-      </h1>
       <ul className="plan__list">{children}</ul>
     </div>
     <h3
@@ -24,9 +20,6 @@ const Plan = ({ special, icon, name, price, priceMonthly, time, children }) => (
     >
       BOOK NOW
     </h3>
-    <h4 className="plan__single">
-      SINGLE SESSION: <b>{price}</b> / {time} min
-    </h4>
   </div>
 );
 
